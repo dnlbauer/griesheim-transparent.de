b45a83bbe347a6e5cba2a84e153f5f76af876cea
danijoo <danijob88@googlemail.com>
2022-10-18 10:42:14 +0200

    structured autocomplete code

--- frontend/frontend/static/js/autocomplete.js
+++ frontend/frontend/static/js/autocomplete.js
@@ -1,8 +1,7 @@
 let cachedInput = null  // hold the actual user input
 const selectedClass = "selected"
 
-let autocompleteNavigation = (event, element) => {
-    console.log(cachedInput)
+function autocompleteNavigation(event, searchBox) {
     let elements = up.element.all(".autocomplete-item")
 
     // find currently selected element
@@ -12,51 +11,52 @@ let autocompleteNavigation = (event, element) => {
         currentElementIdx = Array.prototype.indexOf.call(elements, currentElement)
     }
 
-    if (event.keyCode === 40 || event.keyCode === 38) { // up or down: move to next suggestion
-        console.log("arrow key pressed")
-        let add
-        if(event.keyCode === 40)
-            add = 1
-        else
-            add = -1
-        let nextElementIdx = Math.min(Math.max(-1, currentElementIdx + add), elements.length-1)
-        if (nextElementIdx !== currentElementIdx) {
-            if (nextElementIdx >= 0) {
-                let newSelectedElement = elements[nextElementIdx]
-                let newValue = newSelectedElement.textContent
-                up.element.toggleClass(newSelectedElement, selectedClass)
-                console.log(`Selected ${newValue}`)
-                if (!cachedInput)
-                    cachedInput = element.value
-                element.value = newValue
-            } else { // -1
-                element.value = cachedInput
-            }
-            if (currentElement) {
-                up.element.toggleClass(currentElement, selectedClass)
-            }
-            event.preventDefault()
-        }
-    } else if(event.keyCode === 27) { // escape: hide suggestions
+    if (event.keyCode === 40)  // arrow down
+        incrSelection(1, currentElement, currentElementIdx, elements, searchBox)
+    else if (event.keyCode === 38)  // arrow up
+        incrSelection(-1, currentElement, currentElementIdx, elements, searchBox)
+    else if(event.keyCode === 27) { // escape: hide suggestions
         hideSuggestions()
         event.preventDefault()
-    } else {
+    } else {  // normal letter: reset cached input
         cachedInput = null
     }
 }
 
-let hideSuggestions = function() {
+function incrSelection(incr, currentElement, currentElementIdx, elements, searchBox) {
+    let nextElementIdx = Math.min(Math.max(-1, currentElementIdx + incr), elements.length-1)
+    if (nextElementIdx !== currentElementIdx) {
+        if (nextElementIdx >= 0) {
+            let newSelectedElement = elements[nextElementIdx]
+            let newValue = newSelectedElement.textContent
+            up.element.toggleClass(newSelectedElement, selectedClass)
+            console.log(`Selected ${newValue}`)
+            if (!cachedInput)
+                cachedInput = searchBox.value
+            searchBox.value = newValue
+        } else { // -1
+            searchBox.value = cachedInput
+        }
+        if (currentElement) {
+            up.element.toggleClass(currentElement, selectedClass)
+        }
+        event.preventDefault()
+    }
+}
+
+function hideSuggestions() {
     up.element.hide(up.element.get(".autocomplete-list"))
     up.element.toggleClass(up.element.get('input'), 'search-box-active', false)
 }
 
-let loadSuggestions = function (value) {
-    if (value.length < 3)
+function loadSuggestions(value) {
+    if (value.length <= 3) // no suggestions for < 3 letters
         return
-    console.log(`Suggest for ${value}`)
-    up.render({target: '.autocomplete-list', url: `/suggest?query=${value}`}).then(() => {
-        up.element.toggleClass(up.element.get('input'), 'search-box-active', true)
-    }).catch(error => {hideSuggestions()})
+    console.log(`Loading suggestions: ${value}`)
+    // load suggestions, change search box style; hide suggestions if loading failed
+    up.render({target: '.autocomplete-list', url: `/suggest?query=${value}`})
+        .then(up.element.toggleClass(up.element.get('input'), 'search-box-active', true))
+        .catch(hideSuggestions())
 }
 
 up.on('keydown', 'input', autocompleteNavigation)

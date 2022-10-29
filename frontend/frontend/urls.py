b34de03287a4c49ee1bc5b8068c3cc3f99864d5c
danijoo <danijob88@googlemail.com>
2022-10-29 12:56:03 +0200

    rename why to faq

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("", MainView.as_view(), name="main"),
     path("search", SearchView.as_view(), name="search"),
     path("suggest", SuggestView.as_view(), name="suggest"),
-    path("why", TemplateView.as_view(template_name="why.html"), name="why"),
+    path("faq", TemplateView.as_view(template_name="faq.html"), name="faq"),
     path("impressum", TemplateView.as_view(template_name="impressum.html"), name="impressum"),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
 ]

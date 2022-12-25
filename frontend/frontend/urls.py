db379ae20bfe621019e6a9fb1778c3c68ce3559b
daniel <github@dbauer.me>
2022-12-25 21:14:49 +0100

    feat: about / impressum style

--- frontend/frontend/urls.py
+++ frontend/frontend/urls.py
@@ -25,7 +25,8 @@ urlpatterns = [
     path("", MainView.as_view(), name="main"),
     path("search", SearchView.as_view(), name="search"),
     path("suggest", SuggestView.as_view(), name="suggest"),
-    path("faq", TemplateView.as_view(template_name="faq.html"), name="faq"),
+    path("about", TemplateView.as_view(template_name="about.html"), name="about"),
+    path("faq", TemplateView.as_view(template_name="about.html"), name="faq"),
     path("impressum", TemplateView.as_view(template_name="impressum.html"), name="impressum"),
     path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
 ]

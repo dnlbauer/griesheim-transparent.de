4461b09f9faa5063130b412f72dd7eb629e1984e
daniel <github@dbauer.me>
2022-12-29 23:56:06 +0100

    feat: more synonyms

--- deployment/update_solr.sh
+++ deployment/update_solr.sh
@@ -2,6 +2,6 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-for f in schema.xml solrconfig.xml; do
+for f in synonyms.txt schema.xml solrconfig.xml; do
   docker cp $SCRIPT_DIR/../solr/configsets/ris/conf/$f griesheimtransparent-solr:/var/solr/data/ris/conf/$f
 done

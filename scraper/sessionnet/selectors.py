4193231116ee86f8826fd130516ec460f6f6f69d
danijoo <danijob88@googlemail.com>
2023-04-10 16:50:49 +0200

    cleanup

--- scraper/sessionnet/selectors.py
+++
@@ -1,3 +0,0 @@
-selector_mandate_row = "//tbody/tr/td"
-selector_organizations_row = "//tbody/tr/td"
-selector_meeting_agenda_link = "//tr[contains(@class, 'smc-t-r-l')]//a[contains(@class, 'smc_datatype_t')]/@href"
--- scraper/sessionnet/settings.py
+++ scraper/sessionnet/settings.py
@@ -34,31 +34,13 @@ ROBOTSTXT_OBEY = False
 #COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
     "Accept-Language": "de; en",
 }
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    "sessionnet.middlewares.SessionnetSpiderMiddleware": 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "sessionnet.middlewares.SessionnetDownloaderMiddleware": 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    "scrapy.extensions.telnet.TelnetConsole": None,
-#}
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html

8917fe86d8de95723daf1b736c8e73dd486f8a55
Daniel Bauer <4690162+dnlbauer@users.noreply.github.com>
2023-05-01 14:47:06 +0200

    Update update_solr.sh

--- deployment/update_solr.sh
+++ deployment/update_solr.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 for f in synonyms.txt schema.xml solrconfig.xml; do
-  docker cp $SCRIPT_DIR/../solr/configsets/ris/conf/$f griesheimtransparent-solr:/var/solr/data/ris/conf/$f
+  docker cp $1/configsets/ris/conf/$f griesheimtransparent-solr:/var/solr/data/ris/conf/$f
 done

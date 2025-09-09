a4c2d064fd29f508a6df9c4c24103167382987b3
Daniel Bauer <github@dbauer.me>
2025-09-09 15:26:54 +0200

    feat: add container name parameter to solr update script

--- deployment/update_solr.sh
+++ deployment/update_solr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+# Usage: ./update_solr.sh /path/to/project solr_container_name
 
 for f in synonyms.txt schema.xml solrconfig.xml; do
-  docker cp $1/configsets/ris/conf/$f parliscope-solr:/var/solr/data/ris/conf/$f
+  docker cp $1/configsets/ris/conf/$f $2:/var/solr/data/ris/conf/$f
 done

646a01d1924c9fe8063ffe89b18ed86f7759c590
Daniel Bauer <github@dbauer.me>
2025-09-09 16:46:06 +0200

    feat: remove restriction from health check

--- parliscope/parliscope/settings.py
+++ parliscope/parliscope/settings.py
@@ -86,7 +86,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "frontend.middleware.RestrictUserMiddleware",
 ]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"

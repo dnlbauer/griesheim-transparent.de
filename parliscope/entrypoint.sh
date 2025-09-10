cb9a5fdb36dda4a19890af440febe68455dbb516
Daniel Bauer <github@dbauer.me>
2025-09-10 10:43:34 +0200

    fix: django entrypoint should migrate scraped database correctly

--- parliscope/entrypoint.sh
+++ parliscope/entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py collectstatic --no-input --clear
 
 echo "Apply database migrations"
 python manage.py migrate --noinput
-python manage.py migrate --noinput --database=ris
+python manage.py migrate --noinput --database=scraped
 
 if [ "$1" = crond ]; then
   echo "Registering cron job"

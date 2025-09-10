cb757c2fffc9a54934669681c1facc814e3655a0
Daniel Bauer <github@dbauer.me>
2025-09-10 12:21:30 +0200

    feat: store everything in the same database

--- parliscope/entrypoint.sh
+++ parliscope/entrypoint.sh
@@ -5,7 +5,6 @@ python manage.py collectstatic --no-input --clear
 
 echo "Apply database migrations"
 python manage.py migrate --noinput
-python manage.py migrate --noinput --database=scraped
 
 if [ "$1" = crond ]; then
   echo "Registering cron job"
